d796b0359d9723ddedd0013573614ca688574889
edwinfang01 <edwinfang05@gmail.com>
2026-06-25 10:38:36 -0400

    Encapsulate phonetic generation in a function
    Refactor code to encapsulate phonetic generation in a function with error handling for non-alphabetic input.

--- day-26 NATO-alphabet-start/main.py
+++ day-26 NATO-alphabet-start/main.py
@@ -31,5 +31,14 @@ df = pandas.read_csv(ruta_base / 'nato_phonetic_alphabet.csv')
 
 dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-input_word = input("Enter a word: ").upper()
-print([dict[letter] for letter in input_word])
+def generate_phonetic():
+    input_word = input("Enter a word: ").upper()
+    try:
+      output_list = [dict[letter] for letter in input_word]
+    except KeyError:
+        print("sorry only letters in the alphabet please")
+        generate_phonetic()
+    else:
+      print(output_list)
+
+generate_phonetic()
