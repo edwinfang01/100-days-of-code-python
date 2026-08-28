fa54715105dffcd8fc4d5317aa111f982fa70d4f
Edwin Fang <edwinfang05@gmail.com>
2026-08-27 22:49:25 -0400

    Refactor product purchasing logic in main.py
    Updated the cookie clicker script to buy the most expensive product every 15 seconds instead of every 5 seconds.

--- day-48-Cookie-Clicker/main.py
+++ day-48-Cookie-Clicker/main.py
@@ -1,5 +1,4 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,34 +10,40 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://ozh.github.io/cookieclicker/")
 
+# wait for the language selection panel to load
 english_button = WebDriverWait(driver, 5).until(
     EC.visibility_of_element_located((By.ID, "langSelect-EN"))
 )
 english_button.click()
 
+# wait for the cookie button to work
 time.sleep(2)
 cookie_button = driver.find_element(By.CSS_SELECTOR, value="button#bigCookie")
+
+# initialize timer to stop the loop after 5 minutes
 now = time.time()
 timeout = now + 60*5
 while time.time() < timeout:
-    if time.time() - now >= 5:
-        n_of_cookies = int(driver.find_element(By.ID, value="cookies").text.split()[0])
+
+    # buy the most expensive product every 15 seconds
+    if time.time() - now >= 15:
         products = driver.find_elements(By.CSS_SELECTOR, value="#products > div.product")
 
-        highest_price = 0
-        highest_price_product = None
-        for product in products:
-            product_price = int(product.find_element(By.CSS_SELECTOR, value=".price").text.replace(",", "")) if product.find_element(By.CSS_SELECTOR, value=".price").text else 0
-            if n_of_cookies > product_price > highest_price:
-                highest_price = product_price
-                highest_price_product = product
+        best_product = None
+        # check each product from most expensive to cheapest
+        for product in reversed(products):
+            if "enabled" in product.get_attribute("class"):
+                best_product = product
+                break
 
-        if highest_price_product:
-            highest_price_product.click()
-
+        if best_product:
+            best_product.click()
+        # reset timer to count to 15 seconds again
         now = time.time()
 
     cookie_button.click()
 
 cookies_per_sec = driver.find_element(By.ID, value="cookiesPerSecond").text
 print(cookies_per_sec)
+
+driver.quit()
