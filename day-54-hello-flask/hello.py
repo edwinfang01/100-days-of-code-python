e432ef37ec3f6a0835ea75ccbddcce1d917d58cc
Edwin Fang <edwinfang05@gmail.com>
2026-09-11 11:52:27 -0400

    Add files via upload

---
+++ day-54-hello-flask/hello.py
@@ -0,0 +1,18 @@
+import random
+
+from flask import Flask
+
+app = Flask(__name__)
+print(random.__name__)
+print(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/bye")
+def say_bye():
+    return "Bye"
+
+if __name__ == "__main__":
+    app.run()
